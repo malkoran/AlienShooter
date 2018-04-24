5ab254ac803f26693c87f72e7274d4e7bcc92687
Malkoran <makotkin98@mail.ru>
2018-04-24 20:11:48 +0300

    Add files via upload

---
+++ maps/bloks.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="image" tilewidth="32" tileheight="32" tilecount="198" columns="18">
+ <image source="../images/image.png" trans="000000" width="576" height="352"/>
+</tileset>
---
+++ maps/sprite.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="sprite" tilewidth="32" tileheight="32" tilecount="8" columns="8">
+ <image source="../images/sprite.png" trans="ffffff" width="256" height="32"/>
+</tileset>
